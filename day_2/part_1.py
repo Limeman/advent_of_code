a9c1ba3caf924804fe6e2921366cbe103b3f90a1
Limeman <Loepolepe@gmail.com>
2021-12-02 20:23:31 +0100

    Day 2 done, yay!

--- day_2/part_1.py
+++ day_2/part_1.py
@@ -1,7 +1,29 @@
 import sys
 
+def forward(pos, steps):
+    return (pos[0] + steps, pos[1])
+
+def down(pos, steps):
+    return (pos[0], pos[1] + steps)
+
+def up(pos, steps):
+    return (pos[0], pos[1] - steps)
+
+
 def main():
-    pass
+    curr_pos = (0, 0)
+    for line in sys.stdin:
+        [action, str_steps] = line.split()
+        steps = int(str_steps)
+
+        if action == 'forward':
+            curr_pos = forward(curr_pos, steps)
+        elif action == 'down':
+            curr_pos = down(curr_pos, steps)
+        else:
+            curr_pos = up(curr_pos, steps)
+    print("Horzontal pos:\t" + str(curr_pos[0]) + "\nDepth:\t\t" + str(curr_pos[1]))
+    print("Multiplied:\t" + str(curr_pos[0] * curr_pos[1]))
 
 if __name__ == "__main__":
     main()
--- day_2/part_2.py
+++ day_2/part_2.py
@@ -1,7 +1,29 @@
 import sys
 
+def forward(pos, steps):
+    return (pos[0] + steps, pos[1] + steps * pos[2], pos[2])
+
+def down(pos, steps):
+    return (pos[0], pos[1], pos[2] + steps)
+
+def up(pos, steps):
+    return (pos[0], pos[1], pos[2] - steps)
+
+
 def main():
-    pass
+    curr_pos = (0, 0, 0)
+    for line in sys.stdin:
+        [action, str_steps] = line.split()
+        steps = int(str_steps)
+
+        if action == 'forward':
+            curr_pos = forward(curr_pos, steps)
+        elif action == 'down':
+            curr_pos = down(curr_pos, steps)
+        else:
+            curr_pos = up(curr_pos, steps)
+    print("Horzontal pos:\t" + str(curr_pos[0]) + "\nDepth:\t\t" + str(curr_pos[1]) + "\nAim:\t\t" + str(curr_pos[2]))
+    print("Multiplied:\t" + str(curr_pos[0] * curr_pos[1]))
 
 if __name__ == "__main__":
     main()
