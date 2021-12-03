a79eae3245e7dd8e83b3fffd0ee96f2fcb6ff1b6
Emil Hammarlund <emil.hammarlund@hiq.se>
2021-12-03 16:33:59 +0100

    Solved part 2

--- day_3/part_2.py
+++ day_3/part_2.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+from numpy.core.fromnumeric import shape
 
 def get_most_common_number_in_row(bits):
     one_count = np.count_nonzero(bits == 1)
@@ -16,7 +17,14 @@ def get_most_common_number_in_row_with_pos(bits, pos):
     else:
         return 0
 
-def get_least_common_number_in_row(bits, pos):
+def get_least_common_numer_in_row(bits):
+    one_count = np.count_nonzero(bits == 1)
+    zero_count = np.count_nonzero(bits == 0)
+    if zero_count <= one_count:
+        return 0
+    else:
+        return 1
+def get_least_common_number_in_row_with_pos(bits, pos):
     one_count = np.count_nonzero(bits[:, pos] == 1)
     zero_count = np.count_nonzero(bits[:, pos] == 0)
     if zero_count <= one_count:
@@ -25,19 +33,38 @@ def get_least_common_number_in_row(bits, pos):
         return 1
 
 def get_oxygen_generator_rating(bits):
-    print(bits.shape)
     for i in range(bits.shape[1]):
         if i == 0:
             most_common = get_most_common_number_in_row_with_pos(bits, i)
             curr_bits = np.nonzero(bits[:, i] == most_common)
+            continue
         else:
-            most_common = get_most_common_number_in_row(curr_bits)
             vert_bits = bits[:, i]
-            curr_bits = np.nonzero(vert_bits[curr_bits] == most_common)
-        print(curr_bits)
-    return curr_bits
+            most_common = get_most_common_number_in_row(vert_bits[curr_bits])
+            matches = np.nonzero(vert_bits == most_common)
+            curr_bits = np.array(np.intersect1d(curr_bits, matches))
+        
+        if len(curr_bits) == 1:
+            break
+    return binary_to_int(bits[curr_bits[0], :])
 
+def get_co2_scrubber_rating(bits):
+    for i in range(bits.shape[1]):
+        if i == 0:
+            least_common = get_least_common_number_in_row_with_pos(bits, i)
+            curr_bits = np.nonzero(bits[:, i] == least_common)
+            continue
+        else:
+            vert_bits = bits[:, i]
+            least_common = get_least_common_numer_in_row(vert_bits[curr_bits])
+            matches = np.nonzero(vert_bits == least_common)
+            curr_bits = np.array(np.intersect1d(curr_bits, matches))
+        if len(curr_bits) == 1:
+            break
+    return binary_to_int(bits[curr_bits[0], :])
 
+def binary_to_int(binary_array):
+    return binary_array.dot(2**np.arange(binary_array.size)[::-1])
            
         
 
@@ -47,7 +74,9 @@ def main():
         listified_line = [int(x) for x in line if x != "\n"]
         values.append(listified_line)
     bits = np.array(values)
-    print(get_oxygen_generator_rating(bits))
+    oxygen_generator_rating = get_oxygen_generator_rating(bits)
+    co2_generator_rating = get_co2_scrubber_rating(bits)
+    print("Life support rating:\t" + str(oxygen_generator_rating * co2_generator_rating))
 
 if __name__ == "__main__":
     main()
