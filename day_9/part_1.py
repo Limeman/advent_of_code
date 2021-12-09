6340ad568fdca48b7ab29786640c289d1d00e4f5
Emil Hammarlund <emil.hammarlund@hiq.se>
2021-12-09 11:27:24 +0100

    Continuation

--- day_9/part_1.py
+++ day_9/part_1.py
@@ -16,12 +16,9 @@ def get_nth_smallest(a, n):
 def main():
     heightmap = get_heightmap()
     windows = np.lib.stride_tricks.sliding_window_view(np.pad(heightmap, (1, 1), constant_values=(9, 9)), (3, 3))
-    print(windows.shape)
-    row_mask = np.array([True, False, True])
-    col_mask = np.array([True, False, True])
-    print(windows[:, :, row_mask, col_mask])
-    print(windows[:, :, row_mask, col_mask].shape)
-    low_x, low_y = np.nonzero(np.min(windows[:, :, row_mask, col_mask], axis=(2, 3)) == windows[:, :, 1, 1])
+    row_mask = np.array([0, 1, 2, 0, 2, 0, 1, 2])
+    col_mask = np.array([0, 0, 0, 1, 1, 1, 2, 2])
+    low_x, low_y = np.nonzero(np.min(windows[:, :, row_mask, col_mask], axis=2) > windows[:, :, 1, 1])
     print("Sum of low points:\t" + str(np.sum(heightmap[low_x, low_y] + 1)))
 
 if __name__ == "__main__":
