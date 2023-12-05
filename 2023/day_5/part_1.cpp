33b40401060f492d489df0bf9c27c323fc4239cf
Hammarlund <EHAMMARL@volvocars.com>
2023-12-05 19:48:31 +0100

    solved day 5 in the example, but takes too long on the real test

--- 2023/day_5/part_1.cpp
+++ 2023/day_5/part_1.cpp
@@ -3,17 +3,18 @@
 #include <algorithm>
 #include <ctype.h>
 #include <vector>
+#include <map>
 
 struct gardening_map
 {
-    int destination_range_start_;
-    int source_range_start_;
-    int range_length_;
+    long long destination_range_start_;
+    long long source_range_start_;
+    long long range_length_;
 };
 
 struct almanac
 {
-    std::vector<int> seeds_;
+    std::vector<long long> seeds_;
     std::vector<gardening_map> seed_to_soil_;
     std::vector<gardening_map> soil_to_fertilizer_;
     std::vector<gardening_map> fertilizer_to_water_;
@@ -21,11 +22,13 @@ struct almanac
     std::vector<gardening_map> light_to_temperature_;
     std::vector<gardening_map> temperature_to_humidity_;
     std::vector<gardening_map> humidity_to_location_;
+
+    std::vector<std::map<long long, long long>> global_map_;
 };
 
-std::vector<int> extract_numbers(std::string str)
+std::vector<long long> extract_numbers(std::string str)
 {
-    std::vector<int> ret_val;
+    std::vector<long long> ret_val;
     std::string curr_num;
     bool hit_number{false};
     for (auto c : str)
@@ -42,8 +45,7 @@ std::vector<int> extract_numbers(std::string str)
 
         if (hit_number && !isdigit(c))
         {
-            std::cout << curr_num << std::endl;
-            ret_val.push_back(std::stoi(curr_num));
+            ret_val.push_back(std::stoll(curr_num));
             curr_num.clear();
             hit_number = false;
         }
@@ -60,42 +62,111 @@ std::vector<gardening_map> read_gardening_maps()
 {
     std::vector<gardening_map> ret_val;
     std::string curr;
-    while (std::getline(std::cin, curr) && curr != "\n")
+    while (std::getline(std::cin, curr) && curr != "")
     {
-        std::cout << curr << std::endl;
         gardening_map tmp;
-        std::vector<int> numbers = extract_numbers(curr);
+        std::vector<long long> numbers = extract_numbers(curr);
         tmp.destination_range_start_ = numbers[0];
         tmp.source_range_start_ = numbers[1];
         tmp.range_length_ = numbers[2];
+        ret_val.push_back(tmp);
     }
     return ret_val;
 }
 
-int main()
+std::map<long long, long long> get_mapping(std::vector<gardening_map> arg)
 {
+    std::map<long long, long long> ret_val;
+    for (auto g : arg)
+    {
+        for (int i = 0; i < g.range_length_; i++)
+        {
+            ret_val.insert({g.source_range_start_ + i, g.destination_range_start_ + i});
+        }
+    }
+    return ret_val;
+}
+
+almanac read_almanac()
+{
+    almanac ret_val;
     std::string curr;
-    int sum = 0;
-    almanac almanac_;
     while (std::getline(std::cin, curr))
     {
         if (curr.rfind("seeds:", 0) == 0)
         {
-            almanac_.seeds_ = extract_numbers(curr.substr(curr.find(":")));
+            ret_val.seeds_ = extract_numbers(curr.substr(curr.find(":")));
         }
 
         if (curr.rfind("seed-to-soil", 0) == 0)
         {
-            almanac_.seed_to_soil_ = read_gardening_maps();
+            ret_val.seed_to_soil_ = read_gardening_maps();
+            ret_val.global_map_.push_back(get_mapping(ret_val.seed_to_soil_));
+        }
+
+        if (curr.rfind("soil-to-fertilizer", 0) == 0)
+        {
+            ret_val.soil_to_fertilizer_ = read_gardening_maps();
+            ret_val.global_map_.push_back(get_mapping(ret_val.soil_to_fertilizer_));
+        }
+
+        if (curr.rfind("fertilizer-to-water", 0) == 0)
+        {
+            ret_val.fertilizer_to_water_ = read_gardening_maps();
+            ret_val.global_map_.push_back(get_mapping(ret_val.fertilizer_to_water_));
+        }
+
+        if (curr.rfind("water-to-light", 0) == 0)
+        {
+            ret_val.water_to_light_ = read_gardening_maps();
+            ret_val.global_map_.push_back(get_mapping(ret_val.water_to_light_));
+        }
+
+        if (curr.rfind("light-to-temperature", 0) == 0)
+        {
+            ret_val.light_to_temperature_ = read_gardening_maps();
+            ret_val.global_map_.push_back(get_mapping(ret_val.light_to_temperature_));
+        }
+
+        if (curr.rfind("temperature-to-humidity", 0) == 0)
+        {
+            ret_val.temperature_to_humidity_ = read_gardening_maps();
+            ret_val.global_map_.push_back(get_mapping(ret_val.temperature_to_humidity_));
+        }
+
+        if (curr.rfind("humidity-to-location", 0) == 0)
+        {
+            ret_val.humidity_to_location_ = read_gardening_maps();
+            ret_val.global_map_.push_back(get_mapping(ret_val.humidity_to_location_));
         }
     }
+    return ret_val;
+}
 
-    for (auto elem : almanac_.seed_to_soil_)
+long long find_closest_location(almanac arg)
+{
+    long long closest_location = 0xFFFFFF;
+    for (auto s : arg.seeds_)
     {
-        std::cout << elem.destination_range_start_ << " " << elem.source_range_start_ << " " << elem.range_length_ << " ";
+        long long curr = s;
+        for (auto g : arg.global_map_)
+        {
+            if (g.find(curr) != g.end())
+            {
+                curr = g[curr];
+            }
+        }
+        if (curr < closest_location)
+        {
+            closest_location = curr;
+        }
     }
-    std::cout << "\n";
+    return closest_location;
+}
 
-    std::cout << "The answer is " << sum << std::endl;
+int main()
+{
+    almanac almanac_ = read_almanac();
+    std::cout << "The answer is " << find_closest_location(almanac_) << std::endl;
     return 0;
 }
